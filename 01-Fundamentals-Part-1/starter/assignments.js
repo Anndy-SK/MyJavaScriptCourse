c541d6c9948a61db158d809f70fd4bec36e1ef62
Andrej Klein <andrejklein123@gmail.com>
2023-07-02 18:54:15 +0200

    first commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,14 @@
+/*
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+let country = "Slovakia";
+let continent = "Europe";
+let population = 5000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
