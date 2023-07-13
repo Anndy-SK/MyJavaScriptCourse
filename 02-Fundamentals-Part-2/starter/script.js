e75229c3bf58b7cb85bf7f3f0d50942adbc9f2cd
Andrej Klein <andrejklein123@gmail.com>
2023-07-13 22:48:38 +0200

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -43,7 +43,7 @@ const num = Number('23');
 
 /* Lecture: Function Declarations vs. Expressions
 // Function declaration:
-function calcAge1(birthYear){
+function calcAge1(birthYear) {
     // const age = 2037 - birthYear;
     // return age;
     return 2037 - birthYear;
@@ -53,11 +53,47 @@ const age1 = calcAge1(1991);
 // console.log(age1);
 
 // Function expression:
-const calcAge2 = function (birthYear){
+const calcAge2 = function (birthYear) {
     return 2037 - birthYear;
 }
 const age2 = calcAge2(1991);
+// console.log(age1, age2);
 
-console.log(age1, age2);
+// Lecture: Arrow Functions
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+// const yearsUntilRetirement = birthYear => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return retirement;
+// }
+// console.log(yearsUntilRetirement(1991));
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
 */
 
+
+// Lecture: Functions calling other functions
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+
+// Lecture: Reviewing functions
