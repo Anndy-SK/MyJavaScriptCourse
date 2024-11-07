2eeeea5b79aec731ff9aa9fb3dfad5e1de58cda1
AnndySK <andrejklein123@gmail.com>
2024-11-07 10:28:11 +0100

    Update challenge3.js

--- 11-Arrays-Bankist/starter/challenge3.js
+++ 11-Arrays-Bankist/starter/challenge3.js
@@ -10,7 +10,7 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-const calcAverageHumanAge = function (ages) {
+const calcAverageHumanAge2 = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   //   console.log(humanAges);
   const adults = humanAges.filter(age => age >= 18);
@@ -22,6 +22,13 @@ const calcAverageHumanAge = function (ages) {
 
   return average;
 };
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+// adults.length
 
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
