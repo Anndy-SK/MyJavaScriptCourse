7c72362cf14cfb9746144541e296345ec428ab46
Andrej Klein <andrejklein123@gmail.com>
2024-11-15 11:30:39 +0100

    192. Event Propagation in Practice

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -160,7 +160,7 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 
 ////////////////////////////////// Lesson 190.
-
+/*
 const h1 = document.querySelector('h1');
 
 // h1.addEventListener('mouseenter', function (e) {
@@ -180,5 +180,37 @@ const alertH1 = function (e) {
 h1.addEventListener('mouseenter', alertH1);
 
 setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+*/
+////////////////////////////////// Lesson 192.
 
-////////////////////////////////// Lesson 191.
+// rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)},${randomInt(0, 255)})`;
+// console.log(randomColor(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  // console.log('LINK');
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // console.log('LINK');
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    // console.log('LINK');
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  } //, true
+);
