5644d2868b32fde4eb76f20fb9161285bb54f789
Andrej Klein <andrejklein123@gmail.com>
2024-11-28 11:26:06 +0100

    217 object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -162,6 +162,7 @@ const walter = new PersonCl('Walter White', 1965);
 
 PersonCl.hey();
 
+/*
 const account = {
   owner: 'Jonas',
   movements: [200, 530, 120, 300],
@@ -179,3 +180,28 @@ console.log(account.latest);
 
 account.latest = 50;
 console.log(account.movements);
+*/
+/////////////////////////////////////// 217.
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__);
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
